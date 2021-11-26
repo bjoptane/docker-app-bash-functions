f6dc6b83d13662217c7aa0f8317aa28e20dae88b
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-11-26 15:29:01 +0000

    travis change

--- python/setup.py
+++ python/setup.py
@@ -6,4 +6,7 @@ from distutils.core import setup, Extension
 name = "zcrypto"
 ver = "0.1"
 
-pydir = os.path.dirname(os.path.abspath(_
+pydir = os.path.dirname(os.path.abspath(__file__))
+rootdir = os.path.dirname(pydir)
+libdir = os.path.join(rootdir, name)
+src = ["../{}/{}".format(name, x) for
