3f8561c2628621138a7689ee01637c627d7a4072
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-11-26 16:09:42 +0000

    x86/mce: Fix check for processor context when machine check was taken.

--- python/setup.py
+++ python/setup.py
@@ -9,4 +9,5 @@ ver = "0.1"
 pydir = os.path.dirname(os.path.abspath(__file__))
 rootdir = os.path.dirname(pydir)
 libdir = os.path.join(rootdir, name)
-src = ["../{}/{}".format(name, x) for
+src = ["../{}/{}".format(name, x) for x in os.listdir(libdir) if x.endswith(".c")]
+src.extend([x for x in os.listdir(pydir) if x.endswith(".c")])
