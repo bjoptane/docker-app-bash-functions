2b61b1b4493fab64abbe9d2e36621843106581a0
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-11-30 22:24:15 +0000

    cl-commit

--- test/base64_test.py
+++ test/base64_test.py
@@ -5,4 +5,6 @@ import random
 import sys
 
 def generate(limit=1000):
-    n = rand
+    n = random.randint(limit / 4, limit)
+    dd = bytes(bytearray(random.randint(0, 255) for _ in range(n)))
+    bb = base64.b64enco
