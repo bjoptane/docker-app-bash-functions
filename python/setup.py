f760b1b314f195ded22667da59405bd68f15af5d
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-11-30 14:17:56 +0000

    Update module3.py

--- python/setup.py
+++ python/setup.py
@@ -11,3 +11,9 @@ rootdir = os.path.dirname(pydir)
 libdir = os.path.join(rootdir, name)
 src = ["../{}/{}".format(name, x) for x in os.listdir(libdir) if x.endswith(".c")]
 src.extend([x for x in os.listdir(pydir) if x.endswith(".c")])
+print(src)
+
+setup(
+    name = name,
+	version = ver,
+	ext_modules = [Extension(name, src, include_dirs=[rootd
