4195ea8e390a3957b5307c05482ea4712874cc52
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-12-01 10:20:04 +0000

    renamed scripts

--- test/base64_test.py
+++ test/base64_test.py
@@ -7,4 +7,9 @@ import sys
 def generate(limit=1000):
     n = random.randint(limit / 4, limit)
     dd = bytes(bytearray(random.randint(0, 255) for _ in range(n)))
-    bb = base64.b64enco
+    bb = base64.b64encode(dd).decode("ascii")
+    hh = "".join("{:02x}".format(x) for x in dd)
+    print("H", hh)
+    print("B", bb)
+
+for _ in range(10
