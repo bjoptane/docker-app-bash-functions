2db108845d37c500f56c4568089851b2c8a5ca12
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-12-07 19:31:50 +0000

    More cleanup.

--- test/rsa_naive_test.py
+++ test/rsa_naive_test.py
@@ -23,4 +23,7 @@ print("## tests:")
 for i in range(100):
     m = "0x00" + "".join(["{:02x}".format(random.randint(0, 255)) for _ in range(LEN - 1)])
     c = key.encrypt(int(m, 16), "")
-  
+    c = hex(c[0])
+    c = "0x" + "0" * (LEN * 2 + 2 - len(c)) + c[2:]
+    print("M =", m)
+    print("C =", c)
