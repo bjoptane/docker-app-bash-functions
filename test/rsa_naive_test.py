c8ea24ae72fff151fc164d2271a72a6fb989f670
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-12-07 06:57:23 +0000

    Update NOTES.md

--- test/rsa_naive_test.py
+++ test/rsa_naive_test.py
@@ -15,4 +15,10 @@ n = hex(pkey.n)
 n = "0x" + "0" * (LEN * 2 + 2 - len(n)) + n[2:]
 print("N =", n)
 d = hex(key.key.d)
-d = "0x" + "0" * (LEN * 2 + 2 - len(d
+d = "0x" + "0" * (LEN * 2 + 2 - len(d)) + d[2:]
+print("D =", d)
+print()
+
+print("## tests:")
+for i in range(100):
+    m = "0x00" + "".j
