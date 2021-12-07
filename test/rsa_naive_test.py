12e692c5e88e2627988b62b8192895212e9468d3
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-12-07 10:45:25 +0000

    Version incremented to 3.0.4

--- test/rsa_naive_test.py
+++ test/rsa_naive_test.py
@@ -21,4 +21,6 @@ print()
 
 print("## tests:")
 for i in range(100):
-    m = "0x00" + "".j
+    m = "0x00" + "".join(["{:02x}".format(random.randint(0, 255)) for _ in range(LEN - 1)])
+    c = key.encrypt(int(m, 16), "")
+  
