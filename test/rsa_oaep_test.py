9912792b129aed4ab608c67861886cb00813eeaf
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-12-08 21:41:50 +0000

    Cleanup whitespace

--- test/rsa_oaep_test.py
+++ test/rsa_oaep_test.py
@@ -4,4 +4,12 @@ import string
 import sys
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
-from Crypto.Ha
+from Crypto.Hash import SHA256
+
+
+if len(sys.argv) <= 1:
+    print("rsa_oaep_test.py gen | verify")
+    sys.exit(0)
+
+cmd = sys.argv[1]
+i
