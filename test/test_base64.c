d11a3f12fb8062705721e9f7b1497f2334b2fbdf
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-12-22 09:30:45 +0000

    Fix compilation issues.

--- test/test_base64.c
+++ test/test_base64.c
@@ -6,4 +6,10 @@ static inline uint8_t hex2num(char c) {
     if (c >= 'a') {
         return (uint8_t)(c - 'a' + 10);
     } else {
-        return (uint8_
+        return (uint8_t)(c - '0');
+    }
+}
+
+static size_t hex_decode(const char *text, uint8_t *bin) {
+    const char *p = text;
+    ui
