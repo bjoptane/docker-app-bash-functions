c75d97b23129d60214835e1de65a05485b6f106a
bjoptane <105474681+bjoptane@users.noreply.github.com>
2021-12-22 07:44:27 +0000

    Update README

--- test/test_base64.c
+++ test/test_base64.c
@@ -2,4 +2,8 @@
 #include <stdio.h>
 #include "zcrypto/base64.h"
 
-static inline uint8_t
+static inline uint8_t hex2num(char c) {
+    if (c >= 'a') {
+        return (uint8_t)(c - 'a' + 10);
+    } else {
+        return (uint8_
