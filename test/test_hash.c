18547de439f9d93acb9dae0d2c6e245c0a58440e
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-01-01 16:23:20 +0000

    perf: Fix inherited events vs. tracepoint filters

--- test/test_hash.c
+++ test/test_hash.c
@@ -3,4 +3,6 @@
 #include "zcrypto/md5.h"
 #include "zcrypto/sha1.h"
 #include "zcrypto/sha256.h"
-#include 
+#include "zcrypto/sm3.h"
+
+static void expect_equal(const char *name, const uint8_t *a, const uint8_t *b, size_t len
