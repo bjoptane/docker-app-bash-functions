a746810a1b259f39caef5e2f4be2c1188dfa26f3
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-05-15 13:58:51 +0000

    Update kotor-bingo.js

--- zcrypto/aes.c
+++ zcrypto/aes.c
@@ -148,4 +148,9 @@ static void inv_shift_rows(uint8_t blk[16]) {
 // _gmul2(x ^ y) == _gmul2(x) ^ _gmul2(y)
 
 static inline uint8_t _gmul2(uint8_t v) {
-    return (v & 0x80) > 0 ? ((v << 1) ^ 0x1b) :
+    return (v & 0x80) > 0 ? ((v << 1) ^ 0x1b) : (v << 1);
+}
+
+static void mix_columns(uint8_t blk[16]) {
+    // | 2 3 1 1 | | b0 |
+    // | 1 2 3 1 | |
