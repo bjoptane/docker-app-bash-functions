c99391a54e694a14240d09c8f910a4d5059ec4af
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-07-09 05:34:23 +0000

    add missing variable

--- zcrypto/aes.c
+++ zcrypto/aes.c
@@ -264,4 +264,6 @@ static inline void _cbc_decrypt(const uint32_t *rkey, int rd, uint8_t *iv, size_
     }
 }
 
-static inline void _cfb_encrypt(const uint32_t *rkey, int rd, uint8_t 
+static inline void _cfb_encrypt(const uint32_t *rkey, int rd, uint8_t *iv, size_t len, const uint8_t *plain, uint8_t *cipher) {
+    for (size_t i = 0; i < len; i += 16) {
+        aes_blk_encrypt
