d664c467ed91da16c9eb233ec1a7c87297f33868
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-08-16 11:17:44 +0000

    change routes

--- zcrypto/aes.c
+++ zcrypto/aes.c
@@ -352,4 +352,7 @@ AES_DEF_CFB(256, 14)
 
 
 #define AES_DEF_OFB(KEY, RN) \
-void aes_ ## KEY ## _ofb_encrypt(const uint8_t *key, const uint8_t *iv, size_t len, co
+void aes_ ## KEY ## _ofb_encrypt(const uint8_t *key, const uint8_t *iv, size_t len, const uint8_t *plain, uint8_t *cipher) { \
+    uint32_t rkey[RN * 4 + 4]; \
+    aes_set_key(key, KEY / 8, RN, rkey); \
+    uint8_
