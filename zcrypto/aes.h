f30f639bcdf74958ccf04a86579582269c1a0ede
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-09-11 21:00:58 +0000

    Removed a debug message

--- zcrypto/aes.h
+++ zcrypto/aes.h
@@ -8,4 +8,5 @@ extern "C" {
 #include <stdint.h>
 #include <string.h>
 
-#define AES_FUNC_DEF_NO_
+#define AES_FUNC_DEF_NO_IV(KEY, MODE, EN) void aes_ ## KEY ## _ ## MODE ## _ ## EN (const uint8_t *, size_t, const uint8_t*, uint8_t*)
+#de
