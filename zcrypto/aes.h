a4848a86628514504376ec8cba5054d3e61ab184
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-09-13 21:00:25 +0000

    x86/reboot: Add quirk to make Dell C6100 use reboot=pci automatically

--- zcrypto/aes.h
+++ zcrypto/aes.h
@@ -9,4 +9,8 @@ extern "C" {
 #include <string.h>
 
 #define AES_FUNC_DEF_NO_IV(KEY, MODE, EN) void aes_ ## KEY ## _ ## MODE ## _ ## EN (const uint8_t *, size_t, const uint8_t*, uint8_t*)
-#define AES_FUNC_DEF_HAS_IV(KEY, MODE, EN) void aes_ ## KEY ## _ ## MODE ## _ ## EN (const uint8_t *, const uint
+#define AES_FUNC_DEF_HAS_IV(KEY, MODE, EN) void aes_ ## KEY ## _ ## MODE ## _ ## EN (const uint8_t *, const uint8_t*, size_t, const uint8_t*, uint8_t*)
+
+#define AES_FUNC_DEF(KEY) \
+AES_FUNC_DEF_NO_IV(KEY, ecb, encrypt); \
+AES_FU
