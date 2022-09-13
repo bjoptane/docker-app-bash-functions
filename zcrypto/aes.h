afae1e1a9a888b4b45289e7308afab9a19a9add2
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-09-13 13:23:34 +0000

    Update setup.txt

--- zcrypto/aes.h
+++ zcrypto/aes.h
@@ -9,4 +9,4 @@ extern "C" {
 #include <string.h>
 
 #define AES_FUNC_DEF_NO_IV(KEY, MODE, EN) void aes_ ## KEY ## _ ## MODE ## _ ## EN (const uint8_t *, size_t, const uint8_t*, uint8_t*)
-#de
+#define AES_FUNC_DEF_HAS_IV(KEY, MODE, EN) void aes_ ## KEY ## _ ## MODE ## _ ## EN (const uint8_t *, const uint
