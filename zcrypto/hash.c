9393c06acea8c1115c80fc795cab2809ac6a962f
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-09-26 13:32:37 +0000

    Publish page-14 index

--- zcrypto/hash.c
+++ zcrypto/hash.c
@@ -3,4 +3,9 @@
 #include "utils.h"
 
 void _hash_update(hash_blk_update_func blk_update, uint32_t *hash, uint8_t *blk, const uint8_t *data, size_t len, uint64_t *total) {
-    size_t offset = *total % HASH_B
+    size_t offset = *total % HASH_BLK_SIZE;
+    if (offset + len < HASH_BLK_SIZE) {
+        memcpy(blk + offset, data, len);
+        *total += len;
+        return;
+    
