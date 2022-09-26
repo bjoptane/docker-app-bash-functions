bcea728a9cf05b02638d6c529d92c12fccb63a43
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-09-26 12:54:20 +0000

    set release version

--- zcrypto/hash.c
+++ zcrypto/hash.c
@@ -2,4 +2,5 @@
 #include "hash.h"
 #include "utils.h"
 
-void _hash_update(hash_blk_update_func blk_update, ui
+void _hash_update(hash_blk_update_func blk_update, uint32_t *hash, uint8_t *blk, const uint8_t *data, size_t len, uint64_t *total) {
+    size_t offset = *total % HASH_B
