7b3579ea81b2f42c6fa66844573816144e594641
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-10-12 18:35:25 +0000

    Update post.md

--- zcrypto/hash.h
+++ zcrypto/hash.h
@@ -10,3 +10,24 @@ extern "C" {
 #include <string.h>
 
 #define _hash_store_len(EE, LEN, DATA) \
+    do {                               \
+        LEN *= 8;                      \
+        _store_##EE##_u64(LEN, DATA);  \
+    } while (0)
+
+#define _hash_digest(EE, HASH, LEN, DATA)             \
+    do {                                              \
+        for (size_t i = 0; i < LEN; ++i) {            \
+            _store_##EE##_u32(HASH[i], DATA + i * 4); \
+        }                                             \
+    } while (0)
+
+#define HASH_BLK_SIZE 64
+
+typedef void (*hash_blk_update_func)(uint32_t *, const uint8_t *);
+void _hash_update(hash_blk_update_func blk_update, uint32_t *hash, uint8_t *blk, const uint8_t *data, size_t len, uint64_t *total);
+void _hash_done(hash_blk_update_func blk_update, uint32_t *hash, const uint8_t *data, uint64_t total, bool le);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ zcrypto/hashlib.c
@@ -0,0 +1,5 @@
+#include <assert.h>
+#include "hash.h"
+#include "hashlib.h"
+#include "sm3.h"
+#include "md
