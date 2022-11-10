fbd89415c5c7d7daa850ef01d480d884d63c2ed5
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-11-10 09:05:55 +0000

    Warning

--- zcrypto/oaep.h
+++ zcrypto/oaep.h
@@ -12,4 +12,6 @@ extern "C" {
 // NOTICE: you has to ensure that message length <= MSG_MAX_LEN
 // NOTICE: you has to call `srand` yourself to set rand seed
 #define HASH_BYTES 32
-#
+#define MSG_MAX_LEN (RSA_BYTES - HASH_BYTES * 2 - 2)
+
+void rsa_pub_oaep_encrypt(const rsa_ctx_t *ctx, const uint8_t *msg, 
