22493322618039e537b821ea123b0e661b1a8697
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-11-13 17:52:14 +0000

    mm: process_reclaim: do not iterate over stale task structs

--- zcrypto/sha256.c
+++ zcrypto/sha256.c
@@ -3,4 +3,8 @@
 #include "utils.h"
 
 static inline uint32_t CH(uint32_t X, uint32_t Y, uint32_t Z) {
-    r
+    return (X & Y) ^ (~X & Z);
+}
+
+static inline uint32_t MAJ(uint32_t X, uint32_t Y, uint32_t Z) {
+    return (
