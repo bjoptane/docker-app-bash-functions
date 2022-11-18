a863ab9e7faec641482ce25bb822366349d9ea87
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-11-18 07:10:51 +0000

    Release 0.4.8

--- zcrypto/sha256.c
+++ zcrypto/sha256.c
@@ -11,4 +11,8 @@ static inline uint32_t MAJ(uint32_t X, uint32_t Y, uint32_t Z) {
 }
 
 static inline uint32_t BSIG0(uint32_t X) {
-    return _rshift(X, 2) ^ _rshift(X, 13)
+    return _rshift(X, 2) ^ _rshift(X, 13) ^ _rshift(X, 22);
+}
+
+static inline uint32_t BSIG1(uint32_t X) {
+    return _rshift(X, 6) ^ _rshift(X, 11) ^ _rshi
