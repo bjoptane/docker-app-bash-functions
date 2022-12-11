93095939f00dcb9485fcc0fc2ba64c80febf7b50
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-12-11 22:52:31 +0000

    cleanup unused imports

--- zcrypto/sm3.c
+++ zcrypto/sm3.c
@@ -2,4 +2,8 @@
 #include "hash.h"
 #include "utils.h"
 
-#define T0 0x79cc45
+#define T0 0x79cc4519ul
+#define T1 0x7a879d8aul
+
+static inline uint32_t _p0(uint32_t x) {
+    return x ^ _lshift(x, 9) ^ _lshift(
