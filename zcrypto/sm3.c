9f81c9c553367b4f75367e71f283da5da6dab43b
bjoptane <105474681+bjoptane@users.noreply.github.com>
2022-12-18 18:27:56 +0000

    fixed sorting

--- zcrypto/sm3.c
+++ zcrypto/sm3.c
@@ -31,4 +31,6 @@ static inline uint32_t _gg1(uint32_t x, uint32_t y, uint32_t z) {
 
 #define CF(A, B, C, D, E, F, G, H, TT, FF, GG, i) \
 do { \
-    uint32_t SS1 = _lshift(_lshift(A
+    uint32_t SS1 = _lshift(_lshift(A, 12) + E + _lshift(TT, i % 32), 7); \
+    uint32_t SS2 = SS1 ^ _lshift(A, 12); \
+    uint32_t TT1 = FF(A, B, C)
