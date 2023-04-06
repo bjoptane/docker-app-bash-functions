7131827845b9fbaad332c5c420591b4ff75ec983
bjoptane <105474681+bjoptane@users.noreply.github.com>
2023-04-06 15:31:43 +0000

    writeback: Move I_DIRTY_PAGES handling

--- zcrypto/utils.h
+++ zcrypto/utils.h
@@ -89,4 +89,11 @@ static inline void _store_le_u64(const uint64_t x, uint8_t bs[8]) {
 }
 
 static inline void _store_be_u64(const uint64_t x, uint8_t bs[8]) {
-   
+    *(uint64_t*)bs = x;
+}
+
+#endif // __ZCRYPO_BYTE_ORDER__
+
+
+
+static inline uint32_t _lshift(uint32_t x, int n) {
