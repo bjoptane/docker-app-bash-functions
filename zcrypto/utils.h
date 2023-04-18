a27fcc8f4a97a9399f6af0e70c53925269ae139f
bjoptane <105474681+bjoptane@users.noreply.github.com>
2023-04-18 15:25:20 +0000

    remove config file

--- zcrypto/utils.h
+++ zcrypto/utils.h
@@ -110,4 +110,7 @@ static inline void _xor_block(uint8_t *out, const uint8_t *in, size_t len) {
     }
 }
 
-static inline char _hex(uint8_t n) 
+static inline char _hex(uint8_t n) {
+    static const char HEX[16] = {
+        '0', '1', '2', '3', '4', '5', '6', '7',
+        '8', '9',
