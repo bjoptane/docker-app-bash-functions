33d57d173c133fc6e632e2c36799f4fadab602a8
bjoptane <105474681+bjoptane@users.noreply.github.com>
2023-04-21 08:44:33 +0000

    first setup

--- zcrypto/utils.h
+++ zcrypto/utils.h
@@ -123,4 +123,12 @@ static inline void _expand_hex(uint8_t *data, size_t len) {
     for (size_t i = 0; i < len; ++i) {
         size_t j = len - 1 - i;
         uint8_t x = data[j];
-        data[j * 2] = (uint8_t)_hex((x >> 4
+        data[j * 2] = (uint8_t)_hex((x >> 4) & 0xf);
+        data[j * 2 + 1] = (uint8_t)_hex(x & 0xf);
+    }
+}
+
+
+# ifdef __cplusplus
+}
+# endif
